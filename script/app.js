45cc98a0a33f24d694e9cfb090edcf7adda8cce0
khadidja kalkoul <khadidja@air-de-khadidja.home>
2022-01-30 14:35:08 +0100

    first commit

---
+++ script/app.js
@@ -0,0 +1,9 @@
+// la calculatrice est/commence toujours à zéro
+let total = 0;
+
+
+//fonction qui prend deux parametres qui additionne
+function addition(x) {
+    total += x;
+    return total;
+}
