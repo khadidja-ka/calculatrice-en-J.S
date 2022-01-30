bb40c84d699f3eb3e609d57803dca29b52be0529
khadidja kalkoul <khadidja@air-de-khadidja.home>
2022-01-30 14:37:52 +0100

    fonction de calcul

--- script/app.js
+++ script/app.js
@@ -1,9 +1,28 @@
 // la calculatrice est/commence toujours à zéro
 let total = 0;
 
-
-//fonction qui prend deux parametres qui additionne
+//fonction qui prend deux parametres qui additionne,multiplie, soustrait, divise
 function addition(x) {
     total += x;
+    return total;
+}
+
+function addition(x) {
+    total += x;
+    return total;
+}
+
+function soustraction(x) {
+    total -= x;
+    return total;
+}
+
+function division(x) {
+    total /= x;
+    return total;
+}
+
+function multiplication(x) {
+    total *= x;
     return total;
 }
