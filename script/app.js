86e4d1dd4fdad65d3752781c401d46aee325cc8f
khadidja kalkoul <khadidja@air-de-khadidja.home>
2022-02-01 00:00:14 +0100

    modification erreur css et js

--- script/app.js
+++ script/app.js
@@ -1,42 +1,54 @@
-// la calculatrice est/commence toujours à zéro
-let total = 0;
+// // la calculatrice est/commence toujours à zéro
+// let total = 0;
 
-//fonction qui prend deux parametres qui additionne,multiplie, soustrait, divise
-function addition(x) {
-    total += x;
-    return total;
-}
+// //fonction qui prend deux parametres qui additionne,multiplie, soustrait, divise
+// function addition(x) {
+//     total += x;
+//     return total;
+// }
 
-function addition(x) {
-    total += x;
-    return total;
-}
+// function addition(x) {
+//     total += x;
+//     return total;
+// }
 
-function soustraction(x) {
-    total -= x;
-    return total;
-}
+// function soustraction(x) {
+//     total -= x;
+//     return total;
+// }
 
-//divise avec une condition si total = 0 , si c'est le cas attribuer la valeur de x, sinon faire le reste de l'instruction
-function division(x) {
-    if (total === 0) {
-        return (total=x);
-    }else { 
-        total /= x;
-        return total;
-    }
-}
+// //divise avec une condition si total = 0 , si c'est le cas attribuer la valeur de x, sinon faire le reste de l'instruction
+// function division(x) {
+//     if (total === 0) {
+//         return (total=x);
+//     }else { 
+//         total /= x;
+//         return total;
+//     }
+// }
 
-function multiplication(x) {
-    if (total === 0) {
-        return (total = x);
-    }else {
-        total *= x;
-        return total;
-    }
-}
+// function multiplication(x) {
+//     if (total === 0) {
+//         return (total = x);
+//     }else {
+//         total *= x;
+//         return total;
+//     }
+// }
 
-// remettre à zero ma calculatrice
-function reset() {
-    total = 0;
-}
+// // remettre à zero ma calculatrice
+// function reset() {
+//     total = 0;
+// }
+
+//récuperer tout les bouttons de ma calculatrice
+const buttons = document.querySelectorAll(".btn");
+const result = document.getElementById("result");
+
+//Pour chaque bouton
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+    result.textContent = e.target.id;
+        
+    })
+});
